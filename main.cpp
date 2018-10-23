529db68604688816706ceccf15e0736321f6cc2b
romanstrazanec <roman.strazanec007@gmail.com>
2018-10-23 10:34:00 +0200

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include "Window.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    try {
+        Window window(hInstance, 0, 0, 1024, 768, (char *) ("The title of my window"));
+        return (int)(window.show(nCmdShow));
+    } catch (EXCEPTION_REGISTRATION exception_registration) { return 0; }
+}
