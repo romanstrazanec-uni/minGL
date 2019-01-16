1c436e0e24f4f118b1f4299d2bfc5f67ba1c70a4
romanstrazanec <roman.strazanec007@gmail.com>
2019-01-16 08:59:33 +0100

    change a to start, b to end,center

--- Objects/Line.cpp
+++ Objects/Line.cpp
@@ -1,21 +1,41 @@
 #include "Line.h"
 
-Line::Line(): a(Point()), b(Point()){}
-Line::Line(const Point &p): a(Point()), b(p){}
-Line::Line(int x, int y): a(Point()), b(Point(x, y)){}
-Line::Line(const Point &a, const Point &b): a(a), b(b){}
-Line::Line(int ax, int ay, int bx, int by): a(Point(ax, ay)), b(Point(bx, by)){}
+// constructors
+Line::Line(): start(Point()), end(Point()){}
+Line::Line(const Point &end): start(Point()), end(end){}
+Line::Line(const Line &other) = default;
+Line::Line(Line &&) noexcept = default;
+Line::Line(int x, int y): start(Point()), end(Point(x, y)){}
+Line::Line(const Point &start, const Point &end): start(start), end(end){}
+Line::Line(int startx, int starty, int endx, int endy): start(Point(startx, starty)), end(Point(endx, endy)){}
 Line::~Line() = default;
 
-void Line::setA(int x, int y) { a = Point(x, y); }
-void Line::setB(int x, int y) { b = Point(x, y); }
+void Line::setStart(int x, int y) { start = Point(x, y); }
+void Line::setEnd(int x, int y) { end = Point(x, y); }
 
-void Line::move_to(Point a, Point b){ this->a = a; this->b = b; }
-void Line::move_to(int ax, int ay, int bx, int by){ this->a = Point(ax, ay); this->b = Point(bx, by); }
-double Line::length() const {
-    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+void Line::move_to(const Point &start, const Point &end){
+    this->start = start, this->end = end;
+}
+void Line::move_to(int startx, int starty, int endx, int endy){
+    start = Point(startx, starty), end = Point(endx, endy);
 }
 
-bool Line::is_point() const { return a == b; }
-bool Line::operator==(const Line &l){ return a == l.a && b == l.b; }
+double Line::length() const {
+    if(start.x == end.x) return fabs((double)start.y - end.y);
+    if(start.y == end.y) return fabs((double)start.x - end.x);
+    return sqrt(pow(start.x - end.x, 2) + pow(start.y - end.y, 2));
+}
+bool Line::is_point() const { return start == end; }
+Point Line::center() const {
+    if(is_point()) return start;
+    auto x = (int)round((fmax(start.x, end.x) - fmin(start.x, end.x)) / 2);
+    auto y = (int)round((fmax(start.y, end.y) - fmin(start.y, end.y)) / 2);
+    return Point(x, y);
+}
+
+bool Line::operator==(const Line &l){ return start == l.start && end == l.end; }
 bool Line::operator!=(const Line &l){ return !(*this == l); }
+
+
+
+
