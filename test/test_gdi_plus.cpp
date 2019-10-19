3f7137b99039c643ba85fcfa1c60fb5bafd98426
romanstrazanec <roman.strazanec007@gmail.com>
2019-10-19 12:16:06 +0200

    add test gdi plus basic window

---
+++ test/test_gdi_plus.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+#include <gdiplus.h>
+
+LRESULT CALLBACK WindowProcessMessages(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE previousInstance, PSTR cmdLine, INT cmdCount) {
+    Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
+
+    const char *CLASS_NAME = "myWin32WindowClass";
+    WNDCLASS wc{};
+    wc.hInstance = currentInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH) COLOR_WINDOW;
+    wc.lpfnWndProc = WindowProcessMessages;
+    RegisterClass(&wc);
+
+    CreateWindow(CLASS_NAME, "Win32 Tutorial", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
+                 800, 600, nullptr, nullptr, nullptr, nullptr);
+
+    MSG msg{};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    Gdiplus::GdiplusShutdown(gdiplusToken);
+    return 0;
+}
+
+LRESULT CALLBACK WindowProcessMessages(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    switch (msg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, msg, wparam, lparam);
+    }
+}
