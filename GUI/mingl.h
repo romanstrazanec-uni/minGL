2009f57072148d92f76573073dcabc5ee9f82b93
romanstrazanec <roman.strazanec007@gmail.com>
2019-11-04 22:14:51 +0100

    rename include.h to mingl.h

--- GUI/mingl.h
+++ GUI/mingl.h
@@ -1,8 +1,11 @@
-#pragma once
+#ifndef MINGL_H_INCLUDED
+#define MINGL_H_INCLUDED
 
 #define main() WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow)
 
 #include <windows.h>
 #include <string>
 
-#include "../GUI/Window.hpp"
+#include "Window.hpp"
+
+#endif
--- test/test_simple_window.cpp
+++ test/test_simple_window.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include "include.h"
+#include "mingl.h"
 
 using namespace std;
 
