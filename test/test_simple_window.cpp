655b868997236277bbf458476e5dd55368bc3363
romanstrazanec <roman.strazanec007@gmail.com>
2019-11-06 16:39:32 +0100

    refactor test

--- test/test_simple_window.cpp
+++ test/test_simple_window.cpp
@@ -1,17 +1,15 @@
 #include <mingl/mingl.h>
 
-using namespace std;
-
 int main() {
     Window window;
     window.setTitle("title");
-    window.addHandler(MessageHandler(Message::onPaint(), [](HWND h, Message m){
+    window.addHandler(MessageHandler(Message::onPaint(), [](HWND h, Message m) {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(h, &ps);
         FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW + 1));
         EndPaint(h, &ps);
     }));
-    window.addHandler(MessageHandler(Message::onClose(), [](HWND h, Message m){
+    window.addHandler(MessageHandler(Message::onClose(), [](HWND h, Message m) {
         if (MessageBox(h, "Really quit?", "My application", MB_OKCANCEL) == IDOK) DestroyWindow(h);
     }));
     if(!window.create()) return 1;
