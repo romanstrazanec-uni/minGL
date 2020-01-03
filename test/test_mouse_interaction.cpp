7c5ff88496035afb5a88011d8ad98ff43c5e62d2
romanstrazanec <roman.strazanec007@gmail.com>
2020-01-03 18:23:07 +0100

    print coordinates

--- test/test_mouse_interaction.cpp
+++ test/test_mouse_interaction.cpp
@@ -1,9 +1,18 @@
 #include <mingl/mingl.h>
 
+#include <iostream>
+#include <w32api/windowsx.h>
+
+using namespace std;
+
 int main() {
     Window w;
     w.setTitle("Mouse interaction app");
 
-    // todo: finish
+    // prints coordinates
+    w.addHandler(WindowMessage::onMouseMove(), [](Window *, WindowMessage wm) {
+        cout << "(" << GET_X_LPARAM(wm.getLparam()) << ", " << GET_Y_LPARAM(wm.getLparam()) << ")" << endl;
+    });
+
     return w.show();
 }
