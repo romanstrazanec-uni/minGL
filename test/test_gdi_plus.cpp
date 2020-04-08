ac1b70859566ee9bc889e7bbcbafaa1ce80074fc
romanstrazanec <roman.strazanec007@gmail.com>
2020-04-08 23:08:13 +0200

    test gdi+ with minGL

--- test/test_gdi_plus.cpp
+++ test/test_gdi_plus.cpp
@@ -3,10 +3,7 @@
 
 LRESULT CALLBACK WindowProcessMessages(HWND, UINT, WPARAM, LPARAM);
 
-void draw(HDC hdc);
-
 int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE previousInstance, PSTR cmdLine, INT cmdCount) {
-    PROPID p;
     Gdiplus::GdiplusStartupInput gdiplusStartupInput;
     ULONG_PTR gdiplusToken;
     Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
@@ -18,13 +15,14 @@ int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE previousInstance, PSTR c
     wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
     wc.hbrBackground = (HBRUSH) COLOR_WINDOW;
     wc.lpfnWndProc = WindowProcessMessages;
-    RegisterClass(&wc);
 
-    CreateWindow(CLASS_NAME, "Win32 Tutorial", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
+    if (!RegisterClass(&wc)) return -1;
+
+    CreateWindow(CLASS_NAME, "GDI+ Test", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 50,
                  800, 600, nullptr, nullptr, nullptr, nullptr);
 
     MSG msg{};
-    while (GetMessage(&msg, nullptr, 0, 0)) {
+    while (GetMessage(&msg, nullptr, NULL, NULL) > 0) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
@@ -34,28 +32,23 @@ int WINAPI WinMain(HINSTANCE currentInstance, HINSTANCE previousInstance, PSTR c
 }
 
 LRESULT CALLBACK WindowProcessMessages(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-    HDC hdc;
-    PAINTSTRUCT ps;
     switch (msg) {
-        case WM_PAINT:
-            hdc = BeginPaint(hwnd, &ps);
-            draw(hdc);
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            Gdiplus::Graphics gf(BeginPaint(hwnd, &ps));
+            Gdiplus::Pen pen(Gdiplus::Color(255, 255, 0, 0));
+            Gdiplus::SolidBrush brush(Gdiplus::Color(255, 0, 255, 0));
+
+            gf.DrawLine(&pen, 0, 0, 500, 500);
+            gf.FillRectangle(&brush, 400, 200, 100, 100);
+            gf.DrawRectangle(&pen, 450, 400, 100, 150);
             EndPaint(hwnd, &ps);
             return 0;
+        }
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
         default:
             return DefWindowProc(hwnd, msg, wparam, lparam);
     }
-}
-
-void draw(HDC hdc) {
-    Gdiplus::Graphics gf(hdc);
-    Gdiplus::Pen pen(Gdiplus::Color(255, 255, 0, 0));
-    Gdiplus::SolidBrush brush(Gdiplus::Color(255, 0, 255, 0));
-
-    gf.DrawLine(&pen, 0, 0, 500, 500);
-    gf.FillRectangle(&brush, 400, 200, 100, 100);
-    gf.DrawRectangle(&pen, 450, 400, 100, 150);
 }
---
+++ test/test_gdi_plus_mingl.cpp
@@ -0,0 +1,16 @@
+#include <mingl>
+
+int main() {
+    Window window("GDI+ Test", 100, 50, 800, 600);
+
+    window.addOnDrawHandler([](Gdiplus::Graphics *graphics) {
+        Gdiplus::Pen pen(Gdiplus::Color(255, 255, 0, 0));
+        Gdiplus::SolidBrush brush(Gdiplus::Color(255, 0, 255, 0));
+
+        graphics->DrawLine(&pen, 0, 0, 500, 500);
+        graphics->FillRectangle(&brush, 400, 200, 100, 100);
+        graphics->DrawRectangle(&pen, 450, 400, 100, 150);
+    });
+
+    window.show();
+}
