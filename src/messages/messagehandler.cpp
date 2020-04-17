b178defa4ab246e93b7335cfec4096be6ff6e1e2
romanstrazanec <roman.strazanec007@gmail.com>
2020-04-17 05:20:59 +0200

    source messagehandler.cpp

---
+++ src/messages/messagehandler.cpp
@@ -0,0 +1,15 @@
+#include "messagehandler.hpp"
+
+MessageHandler::MessageHandler(WindowMessage &&msg, const Handle &handle)
+        : message(msg), handle(handle) {}
+
+void MessageHandler::handleMessage(Window *window, const WindowMessage &msg) {
+    // Handle only if message codes are equal.
+    if (message == msg) {
+        // Store the params.
+        message = msg;
+        handle(window, message);
+    }
+}
+
+const WindowMessage &MessageHandler::getMessage() const { return message; }
--- src/messages/messagehandler.hpp
+++ src/messages/messagehandler.hpp
@@ -4,54 +4,49 @@
 #include "windowmessage.hpp"
 
 #include <functional>
-#include <utility>
 
 class Window;
 
 typedef std::function<void(Window *, const WindowMessage &)> Handle;
 
+/**
+ * Handles certain message with handle.
+ */
 class MessageHandler {
-    WindowMessage message{};
-    Handle handle{};
+    /**
+     * Message handler handles
+     */
+    WindowMessage message;
+
+    /**
+     * Function
+     */
+    Handle handle;
 
 public:
-    MessageHandler() = default;
+    /**
+     * Cannot be without message and a function.
+     */
+    MessageHandler() = delete;
 
-    MessageHandler(WindowMessage &&msg, Handle handle) // todo: rvalue handle?
-            : message(msg), handle(std::move(handle)) {}
+    /**
+     * Cannot be copied.
+     */
+    MessageHandler(const MessageHandler &) = delete;
 
-    void handleMessage(Window *window, const WindowMessage &msg) { // todo: should have perfect forwarding on msg
-        if (message == msg) {
-            message = msg;
-            handle(window, message);
-        }
-    }
+    MessageHandler(WindowMessage &&, const Handle &);
 
-    const WindowMessage &getMessage() const { return message; }
+    /**
+     * Handles WindowMessage if it equals to the specified.
+     */
+    void handleMessage(Window *, const WindowMessage &);
 
-    /* Static constructors */ // todo: rvalue refs?
-    static MessageHandler onCreate(Handle handle) { return {WindowMessage::onCreate(), std::move(handle)}; }
-    static MessageHandler onPaint(Handle handle) { return {WindowMessage::onPaint(), std::move(handle)}; }
-    static MessageHandler onClose(Handle handle) { return {WindowMessage::onClose(), std::move(handle)}; }
-    static MessageHandler onMouseMove(Handle handle) { return {WindowMessage::onMouseMove(), std::move(handle)}; }
-    static MessageHandler onLeftMouseButtonDown(Handle handle) {
-        return {WindowMessage::onLeftMouseButtonDown(), std::move(handle)};
-    }
-    static MessageHandler onLeftMouseButtonUp(Handle handle) {
-        return {WindowMessage::onLeftMouseButtonUp(), std::move(handle)};
-    }
-    static MessageHandler onMiddleMouseButtonDown(Handle handle) {
-        return {WindowMessage::onMiddleMouseButtonDown(), std::move(handle)};
-    }
-    static MessageHandler onMiddleMouseButtonUp(Handle handle) {
-        return {WindowMessage::onMiddleMouseButtonUp(), std::move(handle)};
-    }
-    static MessageHandler onRightMouseButtonDown(Handle handle) {
-        return {WindowMessage::onRightMouseButtonDown(), std::move(handle)};
-    }
-    static MessageHandler onRightMouseButtonUp(Handle handle) {
-        return {WindowMessage::onRightMouseButtonUp(), std::move(handle)};
-    }
+    /**
+     * Gets the message, which also stores WPARAM and LPARAM.
+     *
+     * @return unmodifiable reference.
+     */
+    const WindowMessage &getMessage() const;
 };
 
 #endif
